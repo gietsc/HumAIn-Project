7546290a61f11dcd853a37909e8a42725767e940
Opaps Ditudidi <opaps.ditudidi@gmail.com>
2021-02-19 09:40:06 +0100

    refactoring

--- test.py
+++
@@ -1,9 +0,0 @@
-from joblib import dump, load
-import Services
-import pandas as pd
-import Models
-import json
-
-import os
-PATH = os.getcwd() + "\\"
-print(Services.define_topic(PATH, 'message.txt', False))
